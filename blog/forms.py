5c5c2a30fbca36e94c687723d8a48b7511814023
Symons <somborisymonss@gmail.com>
2019-01-06 14:08:35 +0300

    Final

--- blog/forms.py
+++ blog/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import Comment
 from .models import Post
 
-class PostForm(forms.ModelForm):
 
+class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', 'link',)
+
 
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ('author', 'text',)
